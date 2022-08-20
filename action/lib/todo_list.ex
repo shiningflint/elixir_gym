0befa18c4dc81f632df079c9810cf97439434a80
Adam Christopher <adamcbw@hey.com>
2022-08-20 11:21:14 +0900

    AutoID feature in TodoList

--- action/lib/todo_list.ex
+++ action/lib/todo_list.ex
@@ -1,11 +1,26 @@
 defmodule TodoList do
-  def new(), do: %{}
+  defstruct auto_id: 1, entries: %{}
+
+  def new(), do: %TodoList{}
 
   def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries = Map.put(
+      todo_list.entries,
+      todo_list.auto_id,
+      entry
+    )
+
+    %TodoList{todo_list |
+      entries: new_entries,
+      auto_id: todo_list.auto_id + 1
+    }
   end
 
   def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
   end
 end
