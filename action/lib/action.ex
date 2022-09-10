eed71f969c73c177db870629cda741309f8a130c
Adam Christopher <adamcbw@hey.com>
2022-09-10 21:58:49 +0900

    Full CRUD TodoList entries

--- action/lib/action.ex
+++ action/lib/action.ex
@@ -7,6 +7,7 @@ defmodule Action do
     TodoList.new()
     |> TodoList.add_entry(%{date: ~D[2022-01-20], title: "Do laundry"})
     |> TodoList.add_entry(%{date: ~D[2022-01-21], title: "Clean the floor"})
+    |> TodoList.add_entry(%{date: ~D[2022-01-21], title: "Watch TeeVee"})
     |> TodoList.add_entry(%{date: ~D[2022-01-22], title: "Movies"})
   end
 end
